da6d429054bb3bcbb9c08200ac2e7582671c808b
PCissewski <cissek99@gmail.com>
2022-03-20 18:21:43 +0100

    Completed component Task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AlertComponent } from './alert/alert.component';
+import { WarningAlert } from './alert/warning/warning.component';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './alert/success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    AlertComponent,
+    WarningAlert,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
